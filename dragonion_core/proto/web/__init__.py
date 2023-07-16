90c8cbb4a1ae63b67d863eafb011b6f9d34b07f9
BarsTiger <kit.barsic@gmail.com>
2023-07-16 23:52:18 +0300

    Added auth and webmessages

---
+++ dragonion_core/proto/web/__init__.py
@@ -0,0 +1,22 @@
+from .webmessage import (
+    webmessage_type_literal,
+    webmessage_error_message_literal,
+    WebMessageMessage,
+    WebErrorMessage,
+    WebNotificationMessage,
+    WebUserMessage,
+    WebMessage,
+    webmessages_union
+)
+
+
+__all__ = [
+    'webmessage_type_literal',
+    'webmessage_error_message_literal',
+    'WebMessageMessage',
+    'WebErrorMessage',
+    'WebNotificationMessage',
+    'WebUserMessage',
+    'WebMessage',
+    'webmessages_union'
+]
