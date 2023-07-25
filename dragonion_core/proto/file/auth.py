ff08c15820be942aeb1ca7ff44373c380436a209
BarsTiger <kit.barsic@gmail.com>
2023-07-25 21:13:22 +0300

    Auth filename recognition

--- dragonion_core/proto/file/auth.py
+++ dragonion_core/proto/file/auth.py
@@ -8,8 +8,11 @@ class AuthFile(sqlitedict.SqliteDict):
     auth - v3 onion auth string in format, that can be written to .auth_private file
     """
     def __init__(self, service):
+        """
+        :param service: Service name or service.auth filename
+        """
         super().__init__(
-            filename=f'{service}.auth',
+            filename=f'{service}.auth' if '.auth' not in service else service,
             tablename='auth',
             autocommit=True
         )
